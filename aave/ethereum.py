adcf6dae1d6fa53f2e1fff46460856b32fc564c0
openVietAnh <vietanhtran.uet@gmail.com>
2025-04-21 15:03:16 +0700

    Fetch financial daily snapshots from The Graph Subgraph

---
+++ aave/ethereum.py
@@ -0,0 +1,60 @@
+import requests
+import csv
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+START_TIME ="1643759930"
+URL = f'https://gateway.thegraph.com/api/{API_KEY}/subgraphs/id/C2zniPn45RnLDGzVeGZCx2Sw3GXrbc9gL4ZfL8B8Em2j'
+
+FIRST_PART_QUERY = """
+{
+  financialsDailySnapshots(
+    first: 1000
+    orderBy: timestamp
+    orderDirection: asc
+    where: {timestamp_gt: \""""
+
+SECOND_PART_QUERY = """\"}
+  ) {
+    totalValueLockedUSD
+    totalDepositBalanceUSD
+    totalBorrowBalanceUSD
+    timestamp
+  }
+}
+"""
+
+keys = None
+current_time = START_TIME
+snapshots = []
+
+while True:
+    query = FIRST_PART_QUERY + str(current_time) + SECOND_PART_QUERY
+    response = requests.post(URL,
+                             '',
+                             json={'query': query})
+    if response.status_code != 200:
+        print("Problem reading from timestamp", current_time, ":", response.status_code)
+        continue
+    try:
+        data = response.json()["data"]["financialsDailySnapshots"]
+    except (AttributeError, KeyError) as error:
+        print("Error at timestamp", current_time)
+        print(error)
+        continue
+    if len(data) == 0:
+        break
+    if keys is None:
+        keys = data[0].keys()
+    print(len(data), "rows found at timestamp", current_time)
+    snapshots += data
+    current_time = int(data[-1]["timestamp"])
+
+with open('aave_ethereum.csv', 'w', newline='') as output_file:
+    DICT_WRITER = csv.DictWriter(output_file, keys)
+    DICT_WRITER.writeheader()
+    DICT_WRITER.writerows(snapshots)
